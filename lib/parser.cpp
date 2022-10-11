244c4e90cc47912211bc1c1791961ccb5146f0b6
Denis <dden4929@gmail.com>
2022-10-11 10:39:13 +0300

    Logic edited.

--- lib/parser.cpp
+++ lib/parser.cpp
@@ -1,17 +1,20 @@
 #include <vector>
-#include <string>
+#include <cstring>
 
-std::vector<int> parse(int argc, char** argv, const std::vector<std::string[2]>& params) {
+std::vector<int> parse(int argc, char** argv, const std::vector<char* [2]>& params) {
     std::vector<int> result = std::vector<int>(params.size());
-    int i = 1;
-    while (i < argc) {
-        std::string arg = argv[i];
+    for (int i = 1; i < argc; i++) {
+        char* arg = argv[i];
+        char* end = arg + sizeof(arg) / sizeof(argv[0]);
+        char* eq = std::find(arg, end, '=');
         for (int j = 0; i < params.size(); i++) {
-            if (arg == params[j][0] || arg == params[j][1]) {
-                result[j] = _atoi64(argv[i++]);
+            if (strcmp(params[j][0], std::substr(arg, eq)) ||
+                strcmp(params[j][1], std::substr(arg, eq))) {
+                result[j] = _atoi64(std::substr(eq + 1, end));
             }
         }
         i++;
     }
+
     return result;
 }
