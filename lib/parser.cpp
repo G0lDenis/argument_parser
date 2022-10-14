102071777af2d35f799a727ef723c76205f68468
Denis <dden4929@gmail.com>
2022-10-14 22:45:02 +0300

    Parameters with and without '-' now supported

--- lib/parser.cpp
+++ lib/parser.cpp
@@ -3,15 +3,24 @@
 
 std::vector<std::string> parse(int argc, char** argv, const std::vector<std::pair<std::string, std::string>>& params) {
     std::vector<std::string> result = std::vector<std::string>(params.size());
-    for (int i = 1; i < argc; i++) {
+    int i = 1;
+    while (i < argc) {
         std::string arg = argv[i];
         auto eq = arg.find('=');
+        std::string value;
+        if (eq == -1) {
+            value = argv[++i];
+        } else {
+            value = arg.substr(eq + 1, arg.length() - eq - 1);
+            arg = arg.substr(0, eq);
+        }
+
         for (int j = 0; j < params.size(); j++) {
-            if (params[j].first == arg.substr(0, eq) ||
-                params[j].second == arg.substr(0, eq)) {
-                result[j] = arg.substr(eq + 1, arg.length() - eq - 1);
+            if (params[j].first == arg || params[j].second == arg) {
+                result[j] = value;
             }
         }
+        i++;
     }
 
     return result;
